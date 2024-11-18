d13af4829d0efe6e44bce1454fdcb280e4ea8cc3
Divyansh Jain <djboss8834@gmail.com>
2024-11-18 22:39:29 +0530

    header done and some fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="p-10 h-screen w-full">
-      <h1 className="text-3xl font-bold text-center text-emerald-900">
-        Welcome to CinemaGPT App!!!
-      </h1>
-    </div>
+    <>
+      <Body />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../assets/CinemaGPT-logo.png";
+
+const Header = () => {
+  return (
+    <div className="w-full h-[10vh] px-6 py-2 flex justify-between items-center bg-gradient-to-b from-black shadow-md">
+      <div className="flex items-center">
+        <img 
+          src={Logo} 
+          alt="CinemaGPT Logo" 
+          className="h-12 sm:h-14" 
+        />
+      </div>
+      
+      <div className="flex items-center gap-4">
+        <select className="border-2 border-white bg-transparent rounded-full px-2 py-1 text-white font-medium text-sm sm:text-base focus:outline-none">
+          <option value="english">English</option>
+          <option value="hindi">Hindi</option>
+        </select>
+        <button className="border-2 border-transparent py-1 px-4 text-sm sm:text-base font-bold text-black bg-white rounded-full">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
