5627062a6c7da21d509f46ca610d707cccc8e7aa
Divyansh Jain <djboss8834@gmail.com>
2024-11-18 20:46:11 +0530

    initial changes in cinema gpt files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Welcome to CinemaGPT App!!!
+    <div className="p-10 h-screen w-full">
+      <h1 className="text-3xl font-bold text-center text-emerald-900">
+        Welcome to CinemaGPT App!!!
+      </h1>
     </div>
   );
 }
